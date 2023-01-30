03b840a5f4543a509cba78f76e079898516415f7
TheLonelyDude <magnus.kongstad@gmail.com>
2023-01-30 16:46:04 +0100

    Update CustomerTest.java

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -1,14 +1,55 @@
+import main.account.BankAccount;
+import main.account.SalaryAccount;
 import main.entities.Customer;
 import main.enums.Status;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CustomerTest {
-
+    Customer customer;
+    @BeforeAll
+    public void init() {
+        customer = new Customer("Test", Status.ADULT);
+    }
     @Test
     public void testCustomerID() {
-        Customer customer = new Customer("Test", Status.ADULT);
         assertEquals(1, customer.getID());
+    }
+
+    @Test
+    public void testCustomerName() {
+        assertEquals("Test", customer.getName());
+    }
+
+    @Test
+    public void testCustomerStatus() {
+        customer.addAccount(new SalaryAccount());
+        assertEquals(Status.ADULT, customer.getStatus());
+    }
+
+    @Test
+    public void testRemoveAccount() {
+        BankAccount account = new SalaryAccount();
+        customer.addAccount(account);
+        assertThrows(IllegalArgumentException.class, () -> customer.removeAccount(account));
+        assertThrows(IllegalArgumentException.class, () -> customer.removeAccount(account));
+    }
+
+    @Test
+    public void testRemoveNullAccount() {
+        assertThrows(IllegalArgumentException.class, () -> customer.removeAccount(null));
+    }
+
+    @Test
+    public void testAddAccount() {
+        BankAccount account = new SalaryAccount();
+        customer.addAccount(account);
+        assertThrows(IllegalArgumentException.class, () -> customer.addAccount(account));
+        assertThrows(IllegalArgumentException.class, () -> customer.addAccount(null));
     }
 }
