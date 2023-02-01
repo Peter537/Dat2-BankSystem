6e4a7f4cfca359de2900ba94c0156f8a6fd85af1
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-01 09:28:24 +0100

    Added transfer invalid test

--- test/TransferTest.java
+++ test/TransferTest.java
@@ -5,12 +5,13 @@ import main.entities.Customer;
 import main.enums.Status;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TransferTest {
 
     @Test
-    public void testTransfer() {
+    public void testTransferValid() {
         Bank bank = new Bank();
         Customer customer1 = new Customer("John", Status.ADULT);
         Customer customer2 = new Customer("Jane", Status.ADULT);
@@ -30,5 +31,29 @@ public class TransferTest {
         bank.addAccount(account2);
 
         assertTrue(bank.transferMoney(account1.getID(), account2.getID(), 500));
+    }
+
+    @Test
+    public void testTransferInvalid() {
+        Bank bank = new Bank();
+        Customer customer1 = new Customer("John", Status.ADULT);
+        Customer customer2 = new Customer("Jane", Status.ADULT);
+
+        bank.addCustomer(customer1);
+        bank.addCustomer(customer2);
+
+        BankAccount account1 = new SalaryAccount();
+        BankAccount account2 = new SalaryAccount();
+
+        account1.setOwner(customer1);
+        account2.setOwner(customer2);
+
+        account1.deposit(1000);
+        account2.deposit(1000);
+
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        assertFalse(bank.transferMoney(account1.getID(), account2.getID(), 1500));
     }
 }
