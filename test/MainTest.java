5e7fdcdafd32db7e4d292e4c0ecc270126c93213
TheLonelyDude <magnus.kongstad@gmail.com>
2023-02-01 09:15:00 +0100

    Ez Thursday assignment

--- test/MainTest.java
+++
@@ -1,6 +0,0 @@
-import main.account.BankAccount;
-
-public class MainTest {
-    public static void main(String[] args) {
-    }
-}
---
+++ test/TransferTest.java
@@ -0,0 +1,34 @@
+import main.Bank;
+import main.account.BankAccount;
+import main.account.SalaryAccount;
+import main.entities.Customer;
+import main.enums.Status;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TransferTest {
+    @Test
+    public void testTransfer() {
+        Bank bank = new Bank();
+        Customer customer1 = new Customer("John", Status.ADULT);
+        Customer customer2 = new Customer("Jane", Status.ADULT);
+        bank.addCustomer(customer1);
+        bank.addCustomer(customer2);
+
+        BankAccount account1 = new SalaryAccount();
+        BankAccount account2 = new SalaryAccount();
+
+        account1.setOwner(customer1);
+        account2.setOwner(customer2);
+
+        account1.deposit(1000);
+        account2.deposit(1000);
+
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        assertTrue(bank.transferMoney(account1.getID(), account2.getID(), 500));
+    }
+}
