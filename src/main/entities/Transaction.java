b86d3f091590394e4af2cbaf01112119077c9de2
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-06 14:42:32 +0100

    Update Transaction.java

--- src/main/entities/Transaction.java
+++ src/main/entities/Transaction.java
@@ -1,15 +1,15 @@
 package main.entities;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 public class Transaction {
 
     private final double amount;
     private final Timestamp timestamp;
 
-    public Transaction(double amount, Timestamp timestamp) {
+    public Transaction(double amount) {
         this.amount = amount;
-        this.timestamp = timestamp;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
     }
 
     public double getAmount() {
