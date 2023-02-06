82fd0aef88ebf576022b1b9def3ab8e9d9b6f908
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-06 14:06:52 +0100

    Update TransferTest.java

--- test/TransferTest.java
+++ test/TransferTest.java
@@ -4,9 +4,11 @@ import main.account.SalaryAccount;
 import main.entities.Customer;
 import main.enums.Status;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TransferTest {
 
     @Test
